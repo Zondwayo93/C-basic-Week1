bc8d48c270fa06dabe2bd37b92c0dbddac26364d
Zondwayo93 <mugalap93@gmail.com>
2025-10-01 01:24:34 +0300

    Day 2: Variables and Data Types

---
+++ Day2/rectangle_area.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int length, width, area;
+    
+    printf("Enter length:");
+    scanf("%d", &length);
+
+    printf("Enter width:");
+    scanf("%d", %width);
+   
+    area = lenth * width;
+
+    printf("Area = %d\n", area);
+
+    return 0;
+}
