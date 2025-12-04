37da4931204676ffbd1e3594a222dde15bbf5920
Zondwayo93 <mugalap93@gmail.com>
2025-12-04 02:35:23 +0200

    Day 2: Largest of three numbers

---
+++ Day2/project7.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a >= b && a >= c) {
+        printf("The largest number is %d\n", a);
+    }
+    if (b >= a && b >= c) {
+        printf("The largest number is %d\n", b);
+    }
+    if (c >= a && c >= b) {
+        printf("The largest number is %d\n", c);
+    }
+
+    return 0;
+}
